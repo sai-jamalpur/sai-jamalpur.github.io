4930ea605176a39083fd6cdc339e59bdc046935c
sai-jamalpur <saij@iiitd.ac.in>
2026-01-27 16:48:59 +0000

    Deploying to gh-pages from @ sai-jamalpur/sai-jamalpur.github.io@901fb8c8f69ec6e84854c8b891dbd3f72eaf9c66 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,16 +23,16 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+        },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-admitted-to-the-phd-program-in-iiitd",
+          title: 'Admitted to the PhD Program in IIITD',
           description: "",
           section: "News",},{
         id: 'social-cv',
